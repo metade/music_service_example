19457d1cd6fd287fee0fe16d27170cc8b1a64e8e
Patrick Sinclair <metade@gmail.com>
2011-06-29 20:35:56 +0100

    Fixing collection tests

--- db/migrate/20110616080550_create_collections.rb
+++ db/migrate/20110616080550_create_collections.rb
@@ -2,12 +2,17 @@ class CreateCollections < ActiveRecord::Migration
   def self.up
     create_table :collections do |t|
       t.string :url_key
-      t.string :title
       t.string :pid
+      t.string :title
+      t.string :artists
       t.string :short_synopsis
       t.string :medium_synopsis
+      t.string :url
+      
       t.integer :user_id
       t.integer :featured_position
+      t.integer :use_pips  # should be boolean
+      t.integer :has_image # should be boolean
       t.datetime :promoted_at
 
       t.timestamps
